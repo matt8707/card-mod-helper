37ea25455f40a7cf6cc5bdb891ad120f0de2d7e9
Mattias Persson <mattias.persson.87@gmail.com>
2022-08-08 05:10:45 +0200

    add card-mod-sidebar-yaml

--- include/script.js
+++ include/script.js
@@ -1,70 +1,108 @@
-// Sample JS path
+const process = () => {
+  // define elements
+  let forminput = document.getElementById("forminput").value,
+    card_mod_type = document.getElementById("card-mod-type");
+
+  // set output type
+  card_mod_type.innerHTML = getType(forminput);
+
+  // run replace
+  replacePath(forminput);
+};
+
+const getType = (forminput) => {
+  // supported types
+  let types = {
+    "card-mod-root-yaml": "#layout > app-header",
+    "card-mod-view-yaml": "#view > hui-view",
+    "card-mod-sidebar-yaml": "#drawer > ha-sidebar",
+    "card-mod-more-info-yaml": "card-tools-popup",
+  };
+
+  // return type
+  for (let [type, path] of Object.entries(types)) {
+    if (forminput.includes(path)) {
+      return `${type}: |`;
+    }
+  }
+  // else return unknown
+  return `card-mod-???`;
+};
+
+const replacePath = (forminput) => {
+  // replace with nothing
+  let x = "";
+
+  // is js path more-info
+  let js_path = forminput.includes("card-tools-popup")
+    ? forminput.replaceAll(
+        `document.querySelector("body > home-assistant").shadowRoot.querySelector("card-tools-popup").shadowRoot.`,
+        x
+      )
+    : forminput
+        .replaceAll(
+          `document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("app-drawer-layout > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.`,
+          x
+        )
+        .replaceAll(
+          `document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("#drawer > ha-sidebar").shadowRoot.`,
+          x
+        );
+
+  // replace characters
+  let replace_char = js_path
+    .replaceAll(`document.`, x)
+    .replaceAll(`querySelector("`, x)
+    .replaceAll(`")`, x)
+    .replaceAll(` `, x)
+    .replaceAll(`>`, `<span class="combinator">&gt;</span>`)
+    .replaceAll(`.shadowRoot.`, `<span class="shadowroot">&#36;</span>`);
+
+  // define output element
+  let output = document.getElementById("outputroot");
+
+  // reset
+  outputroot.innerHTML = x;
+
+  // output converted path
+  if (
+    forminput.includes(
+      `document.querySelector("body > home-assistant").shadowRoot.`
+    )
+  ) {
+    output.innerHTML += `"${replace_char}"`;
+
+    // handle invalid path
+  } else {
+    output.innerHTML += `<span id="error">Invalid JS path</span>`;
+  }
+};
+
 const sample = () => {
-  let sample = 'document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("app-drawer-layout > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("#view > hui-view > hui-masonry-view").shadowRoot.querySelector("#columns > div > hui-entities-card").shadowRoot.querySelector("ha-card")';
+  // define sample path
+  let sample = `document.querySelector("body > home-assistant")\.shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("app-drawer-layout > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("#view > hui-view > hui-masonry-view").shadowRoot.querySelector("#columns > div > hui-entities-card").shadowRoot.querySelector("ha-card")`;
+
+  // set sample path
   document.getElementById("forminput").value = sample;
 };
 
-// Process
-const process = () => {
-    let forminput = document.getElementById("forminput").value;
-
-    // Identify type
-    let card_mod_type = document.getElementById("card-mod-type");
-    card_mod_type.innerHTML = "";
-
-    if (forminput.includes("#layout > app-header")) {
-      card_mod_type.innerHTML += "card-mod-root-yaml: |";
-    } else if (forminput.includes("#view > hui-view")) {
-      card_mod_type.innerHTML += "card-mod-view-yaml: |";
-    } else if (forminput.includes("card-tools-popup")) {
-      card_mod_type.innerHTML += "card-mod-more-info-yaml: |";
-    } else {
-      card_mod_type.innerHTML += "card-mod-???";
-    }
-
-    let input = forminput.includes("card-tools-popup") ? 
-      forminput.replaceAll("document.querySelector(\"body > home-assistant\").shadowRoot.querySelector(\"card-tools-popup\").shadowRoot.", "") : 
-      forminput.replaceAll("document.querySelector(\"body > home-assistant\").shadowRoot.querySelector(\"home-assistant-main\").shadowRoot.querySelector(\"app-drawer-layout > partial-panel-resolver > ha-panel-lovelace\").shadowRoot.querySelector(\"hui-root\").shadowRoot.", "");
-
-    // Replace JS path
-    let js_path = input
-      .replaceAll("document.", "")
-      .replaceAll('querySelector("', "")
-      .replaceAll(" ", "")
-      .replaceAll('")', "")
-      .replaceAll(">", "&gt;")
-      .replaceAll(".shadowRoot.", "&#36;");
-
-    // Root
-    let root = js_path
-      .replaceAll("body>home-assistant&#36;home-assistant-main&#36;app-drawer-layout>partial-panel-resolver>ha-panel-lovelace&#36;hui-root&#36;")
-      .replaceAll("&#36;", "<span class=\"shadowroot\">&#36;</span>")
-      .replaceAll("&gt;", "<span class=\"combinator\">&gt;</span>");
-
-    // Output
-    let outputroot = document.getElementById("outputroot");
-    outputroot.innerHTML = "";
-    if (forminput.includes("document.querySelector(\"body > home-assistant\").shadowRoot.")) {
-      outputroot.innerHTML += "\"" + root + "\"";
-    } else {
-      outputroot.innerHTML += "<span id=\"error\">Invalid JS path</span>";
-    }
-};
-
-// Copy output
 const copy = () => {
-  if (outputroot.innerHTML == "&nbsp;" || outputroot.innerHTML == "<span id=\"error\">Invalid JS path</span>") {} else {
-    const elm = document.getElementById("outputroot");
+  if (
+    outputroot.innerHTML == "&nbsp;" ||
+    outputroot.innerHTML == `<span id="error">Invalid JS path</span>`
+  ) {
+    return;
+  } else {
     if (window.getSelection) {
       const selection = window.getSelection();
       const range = document.createRange();
-      range.selectNodeContents(elm);
+      range.selectNodeContents(document.getElementById("outputroot"));
       selection.removeAllRanges();
       selection.addRange(range);
-      document.execCommand("Copy");
+      document.execCommand("copy");
       window.getSelection().removeAllRanges();
 
-      // Button animation
+      // button animation
       document.getElementById("copy").classList.add("flash");
       window.setTimeout(() => {
         document.getElementById("copy").classList.remove("flash");
@@ -73,15 +111,15 @@ const copy = () => {
   }
 };
 
-// Paste output
 const paste = () => {
-navigator.clipboard.readText()
-  .then(text => {
-    document.getElementById("forminput").value = text;
-    process();
-    copy();
-  })
-  .catch(() => {
-    document.getElementById("forminput").value = "";
-  });
+  navigator.clipboard
+    .readText()
+    .then((text) => {
+      document.getElementById("forminput").value = text;
+      process();
+      copy();
+    })
+    .catch(() => {
+      document.getElementById("forminput").value = "";
+    });
 };
