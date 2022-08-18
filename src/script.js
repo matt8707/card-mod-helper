2391ad6dea075c88faa507f854d4ab6b880d0a49
Mattias Persson <mattias.persson.87@gmail.com>
2022-08-19 01:37:43 +0200

    update

--- src/script.js
+++ src/script.js
@@ -1,16 +1,17 @@
-const process = () => {
-  // define elements
-  let forminput = document.getElementById("forminput").value,
+const convertPath = () => {
+  //define elements
+  let input = document.getElementById("input"),
+    output = document.getElementById("output"),
     card_mod_type = document.getElementById("card-mod-type");
 
   // set output type
-  card_mod_type.innerHTML = getType(forminput);
+  card_mod_type.innerHTML = getType(input);
 
   // run replace
-  replacePath(forminput);
+  replacePath(input, output);
 };
 
-const getType = (forminput) => {
+const getType = (input) => {
   // supported types
   let types = {
     "card-mod-root-yaml": "#layout > app-header",
@@ -21,7 +22,7 @@ const getType = (forminput) => {
 
   // return type
   for (let [type, path] of Object.entries(types)) {
-    if (forminput.includes(path)) {
+    if (input.value.includes(path)) {
       return `${type}: |`;
     }
   }
@@ -29,17 +30,17 @@ const getType = (forminput) => {
   return `card-mod-???`;
 };
 
-const replacePath = (forminput) => {
+const replacePath = (input, output) => {
   // replace with nothing
   let x = "";
 
   // is js path more-info
-  let js_path = forminput.includes("card-tools-popup")
-    ? forminput.replaceAll(
+  let js_path = input.value.includes("card-tools-popup")
+    ? input.value.replaceAll(
         `document.querySelector("body > home-assistant").shadowRoot.querySelector("card-tools-popup").shadowRoot.`,
         x
       )
-    : forminput
+    : input.value
         .replaceAll(
           `document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("app-drawer-layout > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.`,
           x
@@ -58,15 +59,12 @@ const replacePath = (forminput) => {
     .replaceAll(`>`, `<span class="combinator">&gt;</span>`)
     .replaceAll(`.shadowRoot.`, `<span class="shadowroot">&#36;</span>`);
 
-  // define output element
-  let output = document.getElementById("outputroot");
-
   // reset
-  outputroot.innerHTML = x;
+  output.innerHTML = x;
 
   // output converted path
   if (
-    forminput.includes(
+    input.value.includes(
       `document.querySelector("body > home-assistant").shadowRoot.`
     )
   ) {
@@ -78,48 +76,52 @@ const replacePath = (forminput) => {
   }
 };
 
-const sample = () => {
+const samplePath = () => {
   // define sample path
   let sample = `document.querySelector("body > home-assistant")\.shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("app-drawer-layout > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("#view > hui-view > hui-masonry-view").shadowRoot.querySelector("#columns > div > hui-entities-card").shadowRoot.querySelector("ha-card")`;
 
   // set sample path
-  document.getElementById("forminput").value = sample;
+  document.getElementById("input").value = sample;
 };
 
-const copy = () => {
+const copyPath = () => {
+  let output = document.getElementById("output"),
+    copy = document.getElementById("copy");
+
   if (
-    outputroot.innerHTML == "&nbsp;" ||
-    outputroot.innerHTML == `<span id="error">Invalid JS path</span>`
+    output.innerHTML == "&nbsp;" ||
+    output.innerHTML == `<span id="error">Invalid JS path</span>`
   ) {
     return;
   } else {
     if (window.getSelection) {
       const selection = window.getSelection();
       const range = document.createRange();
-      range.selectNodeContents(document.getElementById("outputroot"));
+      range.selectNodeContents(output);
       selection.removeAllRanges();
       selection.addRange(range);
       document.execCommand("copy");
       window.getSelection().removeAllRanges();
 
       // button animation
-      document.getElementById("copy").classList.add("flash");
+      copy.classList.add("flash");
       window.setTimeout(() => {
-        document.getElementById("copy").classList.remove("flash");
+        copy.classList.remove("flash");
       }, 800);
     }
   }
 };
 
-const paste = () => {
+const pastePath = () => {
+  let input = document.getElementById("input");
   navigator.clipboard
     .readText()
     .then((text) => {
-      document.getElementById("forminput").value = text;
-      process();
-      copy();
+      input.value = text;
+      convertPath();
+      copyPath();
     })
     .catch(() => {
-      document.getElementById("forminput").value = "";
+      input.value = "";
     });
 };
